5f673cb9e9e9abb2dfd2fa1a31ce45a448bccda1
Junior Saravia <junior.saravia.o@uni.pe>
2024-07-23 08:49:48 -0500

    upload plugin

--- ios/Sources/ExitAppPlugin/ExitApp.swift
+++ ios/Sources/ExitAppPlugin/ExitApp.swift
@@ -3,6 +3,10 @@ import Foundation
 @objc public class ExitApp: NSObject {
     @objc public func echo(_ value: String) -> String {
         print(value)
+        
+        DispatchQueue.main.async {
+            NotificationCenter.default.post(name: NSNotification.Name("SEND_MESSAGE"), object: nil, userInfo:  ["message": value])
+        }
         return value
     }
 }
